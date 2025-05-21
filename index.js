6873923b8bd62223cc1315e176233ad9138cbbe5
veerapandi2100@gmail.com <veerapandi2100@gmail.com>
2025-05-21 19:40:57 +0530

    Cors enable to secure

--- index.js
+++ index.js
@@ -7,8 +7,30 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware for handling CORS
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:3000'
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) {
+        // Allow requests without origin (Postman, REST clients)
+        return callback(null, true);
+      }
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error(`Origin ${origin} not allowed by CORS`));
+      }
+    },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+  };
+  
+  app.use(cors(corsOptions));
+  
+  
+  
 
 // Middleware for parsing incoming JSON requests
 app.use(express.json());
